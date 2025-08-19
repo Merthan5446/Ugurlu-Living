929069dd54a1975aba66fd2275c320b3b36b478e
Merthan5446 <58476338+Merthan5446@users.noreply.github.com>
2025-08-19 21:43:31 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [], // falls du externe Bilder nutzt, hier Domains eintragen
+  },
+};
+
+module.exports = nextConfig;
