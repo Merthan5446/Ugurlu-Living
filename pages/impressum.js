dfd638936ab0e538c410192e676ffedccc2ad7ff
Merthan5446 <58476338+Merthan5446@users.noreply.github.com>
2025-08-19 21:55:17 +0200

    Create impressum.js

---
+++ pages/impressum.js
@@ -0,0 +1,12 @@
+export default function Impressum() {
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Impressum</h1>
+      <p><b>Serdar Ugurlu</b></p>
+      <p>Kleinunternehmerregelung</p>
+      <p>Sandfahrel 14, 27572 Bremerhaven</p>
+      <p>E-Mail: info@ugurlu-living.de</p>
+      <p>Telefon: 0179 3225317</p>
+    </div>
+  );
+}
